5d4489f41a9e20b7f6ca3ec59319e94d8bbb7caa
xudong.ren <xudong.ren@thoughtworks.com>
2022-04-03 12:35:36 +0800

    pass second test
    Signed-off-by: xudong.ren <xudong.ren@thoughtworks.com>

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -1,11 +1,18 @@
 import java.lang.reflect.InvocationTargetException;
 
 public class Args {
+    private String input;
+
     public Args(String input) {
+        this.input = input;
     }
 
     public <T> T parse(Class<T> optionsClass) {
         try {
+            String[] args = input.split(" ");
+            if (args.length > 1) {
+                return (T) optionsClass.getDeclaredConstructors()[0].newInstance(Integer.valueOf(args[1]));
+            }
             return (T) optionsClass.getDeclaredConstructors()[0].newInstance(true);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException("parse exception", e);
