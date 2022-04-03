6368517e80d131df4c9115958823fd8105d0c2bb
xudong.ren <xudong.ren@thoughtworks.com>
2022-04-03 12:14:17 +0800

    通过第一个测试
    Signed-off-by: xudong.ren <xudong.ren@thoughtworks.com>

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -1,8 +1,19 @@
+import java.lang.reflect.InvocationTargetException;
+
 public class Args {
     public Args(String input) {
     }
 
     public <T> T parse(Class<T> optionsClass) {
+        try {
+            return (T) optionsClass.getDeclaredConstructors()[0].newInstance(true);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
